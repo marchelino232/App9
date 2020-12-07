c6ccb9b6dded56d9530bd0f693445d9fade773dd
Stanford Racasa <sracasa@my365-uwc.ac.za>
2020-12-07 23:00:39 +0200

    T SHIRT APP

---
+++ App9/App9/customer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace App9
+{
+    class customer
+    {
+        [PrimaryKey, AutoIncrement]
+
+        public int ID { get; set; }
+        public string Name { get; set;}
+        public int Size { get; set; }
+        public string Color { get; set; }
+        public string Address { get; set; }      
+        public string Gender { get; set; }
+
+    }
+}
---
+++ App9/App9/database.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+using SQLite;
+
+namespace App9
+{
+    class Database
+    {
+        readonly SQLiteAsyncConnection _database;
+
+        public Database(string dbPath)
+        {
+            _database = new SQLiteAsyncConnection(dbPath);
+            _database.CreateTableAsync<customer>().Wait();
+        }
+        public Task<List<customer>> GetCustomersAsync()
+        {
+            return _database.Table<customer>().ToListAsync();
+        }
+
+        public Task<int> SaveCustomerAsync(customer customer)
+        {
+            return _database.InsertAsync(customer);
+        }
+    }
+    }
